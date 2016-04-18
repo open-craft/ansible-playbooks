62f57f03fd2aca1d3e9754bec944c6457eaa07a8
Jacek Bzdak <jacek@opencraft.com>
2016-04-18 12:19:25 +0200

    Create variables for some constants, add dependend role, key, and conf file are now passed to this role as constants, and not generated from hostname.

--- files/bin/backup-pre/mysql.sh
+++ files/bin/backup-pre/mysql.sh
@@ -4,4 +4,4 @@ TMPDIR=/var/lib/mysql/backup
 
 rm -rf "$TMPDIR"
 mkdir -p "$TMPDIR"
-mysql -e 'show databases' | while read DBNAME; do mysqldump -uroot --complete-insert --events --single-transaction "$DBNAME" > "$TMPDIR/$DBNAME".sql; done
+mysql -s -e 'show databases' | tail -n +1 | grep -v performance_schema | grep -v information_schema | while read DBNAME; do mysqldump -uroot --complete-insert --events --single-transaction "$DBNAME" > "$TMPDIR/$DBNAME".sql; done
---
+++ readme.md
@@ -0,0 +1,9 @@
+Tarsnap role
+============
+
+This role does additional configurations over `pmbauer.tarsnap` role: 
+
+* Creates tarsnap cache directory
+* Uploads tarsnap key 
+
+It also contains tarsnapper configurations we use. 
