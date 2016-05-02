87c0ac353fe33b5a8de376fa706a441f5e66f830
Sven Marnach <sven@marnach.net>
2016-05-02 12:58:39 +0200

    Dump databases separately for backup.

--- templates/backup-pre-postgres.sh
+++ templates/backup-pre-postgres.sh
@@ -4,4 +4,6 @@ TMPDIR="{{ POSTGRES_SERVER_BACKUP_DIR }}"
 
 rm -rf "$TMPDIR"
 mkdir -p "$TMPDIR"
-sudo -u postgres pg_dumpall > "$TMPDIR/postgresql_all_dbs.sql"
+pg_dumpall -U postgres --globals > "$TMPDIR/postgresql_globals.sql"
+psql -U postgres -t -c "SELECT datname FROM pg_database WHERE datistemplate = false;" |
+    xargs -n 1 -I {} pg_dump -U postgres --format=directory --file="$TMPDIR/{}" {}
