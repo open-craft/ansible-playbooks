df8a083902d6a895c3c5cd57dcc789d505e0d645
Jacek Bzdak <jacek@opencraft.com>
2016-05-09 18:03:23 +0200

    Added comment to setup.py.

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
+"""
+This repository is not only an ansible role, but also a home of a python module
+named `sanity_check`. The reason for that is that I needed to have the same checks
+installable as an ansible role, and as a view in [dalite-ng][1]. I decided
+not to copy the code, but find the way to share it between the two,
+and this was the least bad way do do it.
+
+[1]: https://github.com/open-craft/dalite-ng
+"""
+
 from distutils.core import setup
+
 
 setup(
     name='sanity-checker',
