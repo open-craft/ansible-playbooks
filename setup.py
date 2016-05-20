8b19e4b793e473df6c144b18ec42514ad9bfe613
Jacek Bzdak <jacek@opencraft.com>
2016-05-20 13:37:16 +0200

    Fixes a bug in setup.py.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 setup(
     name='sanity-checker',
     version='0.0.1',
-    py_modules='sanity_check',
+    py_modules=['sanity_check'],
     package_dir={'': 'files'},
     license='AGPL',
 )
