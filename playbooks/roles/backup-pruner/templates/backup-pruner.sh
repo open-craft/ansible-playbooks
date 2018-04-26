8585d3eaf801b38c5d1e5600a5bb37fc2f745fdb
Sven Marnach <sven@marnach.net>
2018-04-26 20:34:17 +0200

    Move backup-pruner tasks to proper role.

--- playbooks/roles/backup-pruner/templates/backup-pruner.sh
+++ playbooks/roles/backup-pruner/templates/backup-pruner.sh
@@ -2,11 +2,11 @@
 #!/bin/bash
 # This script runs all of the backup pruners in parallel, returning the output
 # of only the failing pruners.
-# Check {{ JOB_LOG }} for a summary of the most recent pruner run.
-# Check the {{ LOG_DIR }} for the full log of each pruning task.
+# Check {{ BACKUP_PRUNER_JOB_LOG }} for a summary of the most recent pruner run.
+# Check the {{ BACKUP_PRUNER_LOG_DIR }} for the full log of each pruning task.
 set -e
 
-jobs='{% for item in TARSNAPPER_JOBS %}
+jobs='{% for item in BACKUP_PRUNER_TARSNAPPER_JOBS %}
 {{ item.name }}{% endfor %}'
 
 # Buffer the output of each pruner per-task; if the pruner fails (returns a
@@ -15,11 +15,11 @@ jobs='{% for item in TARSNAPPER_JOBS %}
 # The `test ${PIPESTATUS[0]} -eq 0` part is checking the return status of the
 # pruner script; this is necessary because we don't care about the return
 # status of `tee`, which will always be 0.
-task='output=$(/usr/local/sbin/tarsnap-{}.sh expire 2>&1 | tee -a {{ LOG_DIR }}/{}.log ; test ${PIPESTATUS[0]} -eq 0) || (echo "$output" && exit 1)'
+task='output=$(/usr/local/sbin/tarsnap-{}.sh expire 2>&1 | tee -a {{ BACKUP_PRUNER_LOG_DIR }}/{}.log ; test ${PIPESTATUS[0]} -eq 0) || (echo "$output" && exit 1)'
 # Cron will send email if there is any stdout or stderr output regardless of the
 # response code, so by only including output for failed tasks in the output, an
 # email will only be sent out if and only if one or more backup pruners failed.
-echo "$jobs" | tail -n +2 | parallel --no-notice -j0 --joblog {{ JOB_LOG }} $task
+echo "$jobs" | tail -n +2 | parallel --no-notice -j0 --joblog {{ BACKUP_PRUNER_JOB_LOG }} $task
 status=$?
 
 if [ $status -eq 0 ]
