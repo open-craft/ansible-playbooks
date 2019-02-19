d64ac71b3ac7fade6f326adf050db534b0ff7561
Loic Dachary <loic@dachary.org>
2019-02-19 23:57:51 +0100

    vault: implement vault-ssh-signer role

--- molecule/vault/tests/test_vault.py
+++ molecule/vault/tests/test_vault.py
@@ -8,3 +8,16 @@ def test_vault_deploy(host):
     print(cmd.stderr)
     assert json.loads(cmd.stdout)['initialized'] is True
     assert cmd.rc == 0
+
+
+def test_vault_ssh_signer(host):
+    cmd = host.run("""
+    set -ex
+    . /home/ubuntu/.vault-env
+    export VAULT_TOKEN=testtoken
+    vault secrets list | grep '^ssh-signer-backup-pruner/'
+    vault read ssh-signer-backup-pruner/config/ca
+    """)
+    print(cmd.stdout)
+    print(cmd.stderr)
+    assert cmd.rc == 0
