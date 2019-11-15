72272e514744fd0e7f90dbec7ec4ba22acb135e9
Seamus Johnston <seamus@opencraft.com>
2019-11-15 10:44:34 -0600

    Ready load-balancer-v2 for production
    * Handle bootstraping load balancer own cert
    * Remove excessive ansible vars
    * Use certbot role for load-balancer-v2
    * Implement backend mapping for HAProxy
    * Add mechanism for custom certs
    * Implement maint pages for load balancer
      This does two things:
      * adds an extra backend map file to HAProxy -- when domains are
        added to maint.map, they will override the normal mapping
      * creates a set of tags/tasks such that Ansible can be used to
        put sites into and out of maint mode
    * Update README.md

--- playbooks/roles/load-balancer-v2/templates/consul-certificates-render.sh
+++ playbooks/roles/load-balancer-v2/templates/consul-certificates-render.sh
@@ -2,4 +2,4 @@
 
 consul watch -type=keyprefix -prefix={{ haproxy_consul_certs_prefix }} \
     consul lock {{ haproxy_consul_lock_prefix }} \
-        'rm -f {{ haproxy_ocim_certs_dir}}/* && consul kv get -keys {{ haproxy_consul_certs_prefix }}/ | { read -r; while read -r key; do consul kv get "$key" > "{{ haproxy_ocim_certs_dir}}/${key##*/}"; done; } && flock -n -E 0 /tmp/haproxy-reload-lock service haproxy reload'
+        'rm -f /etc/haproxy/certs/ocim/* && consul kv get -keys {{ haproxy_consul_certs_prefix }}/ | { read -r; while read -r key; do consul kv get "$key" > "/etc/haproxy/certs/ocim/${key##*/}"; done; } && flock -n -E 0 /tmp/haproxy-reload-lock service haproxy reload'
