5ca1ed5fae7caef7991d74777fb5ed47cf3eaa21
Matjaz Gregoric <mtyaka@gmail.com>
2020-04-15 09:39:38 +0200

    Introduce cert-manager watch delay.
    The cert-manager should not request certificate right away when DNS
    records for an instance are set. Instead it should wait some time for
    the DNS changes to propagate.

--- playbooks/roles/cert-manager/templates/cert-manager.sh
+++ playbooks/roles/cert-manager/templates/cert-manager.sh
@@ -11,10 +11,12 @@
 cd {{ cert_manager_path }}
 
 consul watch -type=keyprefix -prefix={{ cert_manager_consul_watch_prefix }} \
-    consul lock {{ cert_manager_consul_lock_prefix }} \
-        pipenv run python manage_certs.py {% if cert_manager_stage %}--letsencrypt-use-staging{% endif %} \
-            --contact-email {{ cert_manager_email }} \
-            --log-level {{ cert_manager_log_level }} \
-            --webroot-path {{ cert_manager_webroot_path }} \
-            --consul-ocim-prefix {{ cert_manager_consul_ocim_prefix }} \
-            --consul-certs-prefix {{ cert_manager_consul_certs_prefix }}
+    "sleep {{ cert_manager_watch_delay }}; \
+     consul lock {{ cert_manager_consul_lock_prefix }} \
+         pipenv run python manage_certs.py {% if cert_manager_stage %}--letsencrypt-use-staging{% endif %} \
+             --contact-email {{ cert_manager_email }} \
+             --log-level {{ cert_manager_log_level }} \
+             --webroot-path {{ cert_manager_webroot_path }} \
+             --consul-ocim-prefix {{ cert_manager_consul_ocim_prefix }} \
+             --consul-certs-prefix {{ cert_manager_consul_certs_prefix }} \
+             --dns-delay {{ cert_manager_dns_delay }}"
